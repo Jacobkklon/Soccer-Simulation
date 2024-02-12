d83f070cd96d2929a835a417edc3014c37604387
Jacob Klonsky <jacobklonsky@gmail.com>
2024-02-12 17:46:59 -0500

    Update api stuff.py
    Added a string filtering s.t. we can get column names. The next steps are to get the unique list of column labels in order, get the data in a loop, and write it to a DF. Then we can move it to sql

--- api stuff.py
+++ api stuff.py
@@ -18,7 +18,63 @@ player_table = all_tables[2]
 pre_df_squad = dict()
 features_wanted_squad = {"players_used","games"}
 
-print(player_table)
+player_rows = player_table.find_all('tr')
+
+
+# data = []
+# for row in player_rows:
+#     row_data = []
+#     for cell in row.find_all('td'):
+#         row_data.append(cell.text)
+#     data.append(row_data)
+
+#I'm going to try the method of reading data using the 'data-stat' label, which has a length of 9
+#print(player_rows)
+
+rawd = str(player_rows)
+colname = "" #Name of each column
+colnames = [] #Name of the stat we are looking at
+name_ind = 0 #Index that the colname starts at
+colmode = False #This indicates we are looking for columns
+
+#NOTE - to use quotes in a string you must use \"
+for i in range(10,len(rawd)): #Starting at 10 to avoid index issues
+    
+    if rawd[i-9:i] == "data-stat":
+
+        name_ind = i + 2
+        colmode = True
+
+    
+    elif colmode == True and i >= name_ind: #If we are in column-name finding mode and the column name is present
+        
+        colname += (rawd[i]) #Appending next char
+        if rawd[i + 1] == "\"": #A quote char indicates data-stat value is done
+            
+            colmode = False
+            colnames.append(colname)
+            colname = ""
+
+print(colnames)
+
+        
+        
+
+    
+
+
+        
+
+        
+
+    
+        
+
+
+
+ 
+
+
 
 #The next step is to use the fact that "data-stat" goes in front of any statistic and then the number to extract
 #all the data into a pandas DF 
